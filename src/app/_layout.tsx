68a9f1e42ad2d7c1e68aa836fa94edf3f4f2f764
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-20 22:22:51 -0300

    feat: update tabs layout with global tabBarShowLabel option and improved styling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,41 @@
 import { Tabs } from "expo-router";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "@/theme/color";
 
 export default function Layout() {
-  return <Tabs>
-    <Tabs.Screen name="recipes/index" options={{
-      title: "Recipes",
-      tabBarIcon: ({ color, size }) => (
-        <MaterialCommunityIcons name="book-open" color={color} size={size} />
-      ),
-      headerShown: false,
-    }}
-     />
-     <Tabs.Screen name="index" options={{
+  return <Tabs screenOptions={{
+    headerStyle: {
+      backgroundColor: colors.primary,
+    },
+    headerTintColor: colors.textPrimary,
+    tabBarShowLabel: false,
+    headerShown: false,
+  }}>
+    <Tabs.Screen name="index" options={{
       title: "Home",
       tabBarIcon: ({ color, size }) => (
         <MaterialCommunityIcons name="home" color={color} size={size} />
       ),
-      headerShown: false,
-    }}
-     />
-     <Tabs.Screen name="recipes/[id]" options={{
+    }} />
+    <Tabs.Screen name="snapRecipe" options={{
+      title: "SnapFood",
+      tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons name="plus" color={color} size={size} />
+      ),
+    }} />
+    <Tabs.Screen name="recipes" options={{
+      title: "Recipes",
+      tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons name="book-open" color={color} size={size} />
+      ),
+    }} />
+    <Tabs.Screen name="recipe/[id]" options={{
+      title: "Recipe",
       href: null,
       headerShown: false,
-    }}
-     />
+      tabBarStyle: {
+        display: "none",
+      },
+    }} />
   </Tabs>;
 }
